c765c12c7df3a013e70183be6f9da4b80dd47f09
Videl Smith <cstasy@gmail.com>
2009-09-06 19:01:36 +0200

    Correction du prototype de correction

--- vdownload.h
+++ vdownload.h
@@ -26,7 +26,7 @@ class FenOptions;
 
 class VDownload : public QDialog {
 public:
-    VDownload(QWidget* parent, QString nom, int nbPaquet, FenOptions* opt, bool downloading = true);
+    VDownload(QWidget* parent, QString nom, int nbPaquet, FenOptions* opt);
 
     QString getNomFichier();
     void addBytes(QByteArray bytes);
